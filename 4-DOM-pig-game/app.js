5089caaebd9b943ed135847763dab971d5bf5bd4
Aga <a.kolia.grudzien@gmail.com>
2020-03-05 20:55:18 +0100

    add roll a dice function

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -7,17 +7,32 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-let scores = [0,0];
-let roundScore = 0;
-let activePlayer = 0;
+let scores, roundScore, activePlayer;
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
 
 
-let dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-let x = document.querySelector('#score-0').textContent;
-console.log(x);
+//let x = document.querySelector('#score-0').textContent;
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //random number
+    let dice = Math.floor(Math.random() * 6) + 1;
+
+    //display the result
+    let diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+
+    //update the round score if the rolled number is not 1
+});
